a447a63bf02bf3d1799d694bd7057b8ea8113c58
alexis <alexis.baladon@fing.edu.uy>
2026-06-29 17:21:05 -0300

    Fix: Start using environment variables with host names for orchestrator in Dockerfile

--- orchestation/constants.py
+++ orchestation/constants.py
@@ -1,17 +1,19 @@
+import os
+
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
-HOST = "0.0.0.0"
-PORT = 1232
-DEBUG = True
+HOST = os.getenv("HOST", "0.0.0.0")
+PORT = os.getenv("PORT", 1232)
+DEBUG = os.getenv("DEBUG", True)
 
-RETRIEVAL_HOST = "localhost"
-RETRIEVAL_PORT = "1230"
+RETRIEVAL_HOST = os.getenv("RETRIEVAL_HOST", "localhost")
+RETRIEVAL_PORT = os.getenv("RETRIEVAL_PORT", 1230)
 
-RANKING_HOST = "localhost"
-RANKING_PORT = "1233"
+RANKING_HOST = os.getenv("RANKING_HOST", "localhost")
+RANKING_PORT = os.getenv("RANKING_PORT", 1233)
 
-GENERATION_HOST = "localhost"
-GENERATION_PORT = "1231"
+GENERATION_HOST = os.getenv("GENERATION_HOST", "localhost")
+GENERATION_PORT = os.getenv("GENERATION_PORT", 1231)
